eb26a7b6cbc397731d544c3dea1ab128a8beb7b6
saniyashikalgar07 <shikalgarsm01@gmail.com>
2024-07-07 10:47:00 +0530

    Alphabets

---
+++ Alphabets/table range 17 to 9.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int R=0,C=0;
+printf("\n\n ===================table range 17 to 9==================\n\n");
+
+    for (R=1;R<=10;R++)
+    {
+        for(C=17;C>=9;C--)
+        {
+            printf("%5d" ,R*C);
+        }
+
+        printf("\n");
+
+    }
+    printf("\n\n ====================saniya===================\n\n");
+    getch();
+    return 0;
+}
+
+
+
+
+
